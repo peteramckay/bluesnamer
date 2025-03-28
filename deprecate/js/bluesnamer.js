b48bbf8d454598b24fa30314cec7cbed473e80d5
Peter A. McKay <peter@pmckay.com>
2025-03-28 14:50:46 -0400

    Re-factoring as a single-page app for now...

---
+++ deprecate/js/bluesnamer.js
@@ -0,0 +1,55 @@
+// Stashing some old scripts here for now, all commented out. Will add back in later...
+
+
+
+
+
+// "Sweet Meat" Pete's Ultimate Blues Name Generator
+// Copyright (c) 2013-2025, Peter A. McKay
+// Free to use under MIT license
+
+// Some variables to collect user's name from the form...
+
+
+/*
+
+let govFirst;
+let govMiddle;
+let govLast;
+
+
+// Collect user's gender from the form...
+let gender;
+
+// Arrays with potential female blues names...
+
+let bluesFirstF = ['Alice', 'Aunty,' 'Babygirl', 'Candace', 'Dutchess', 'Elvira', 'Frances', 'Mary', 'Valerie', 'Ida', 'Fannie', 'Shirley', 'Foxy', 'Grandma', 'Carolina', 'Queen', 'Sweetie',   'Miss', 'Sleepy', 'Lady', 'Mama', 'Mary', 'Pickles', 'Sugar', 'Janie', 'Suzie'];
+
+let bluesMiddleF = ['"Pine Tree"', '"Washboard"'', '"Big Money"'];
+
+
+// Arrays with potential male blues names...
+
+let bluesFirstM = ['Frenchie', 'Red', 'Louisiana', 'Alabama', 'Mississippi', 'Tennessee', 'Skinny', 'Bill', 'Junious', 'Sonny', 'Sherman', 'Slim', 'Archie', '\"Blind Boy\"', 'Fat', 'Buddy', 'Sticky', 'Old', 'Texas', 'Mississippi', 'Ugly', 'Happy', 'Boney', 'Curly', '\"Prettyboy\"', 'Jailhouse', 'Red', 'Sweet', 'Bald', 'Skinny', 'Blind', 'Big', 'Yella', 'Toothless', 'Slick', '\"Fat Boy\"', 'Washboard', 'Sleepy', 'Papa', 'Earl', 'Buddy', '\"Lightnin\"'];
+
+let bluesMiddleM = ['\"Slick\"', '\"Sweet Lemon\"', '\"Gumbo\"', 'Azariah', 'Appomattox', 'Lennox', '\"Slim\"'];
+
+// Arrays with potential gender-neutral blues names...
+let bluesFirstN = ['Chickenfoot', 'Gypsy', 'Sticky', 'Happy', 'Boney', 'Curly', '\"Jailhouse\"'];
+
+let bluesMiddleN = ['Slick', 'Lemonade', 'Gumbo', "T-Bone", "Death Letter", "Hot Sauce", "Chickenwire", "Mule Head", "Beef Ribs", "Barbecue", "Bumblebee", "Doc", "Sleepyhead", "Lightnin'", 'Belly', 'Blue', 'Crotch', 'Downtown', 'Knees', 'Knuckles', 'Mess', 'Nose', 'Pants', 'Shoes', 'Teeth', 'Toes', 'Ashy', 'City', 'Country', 'Deacon', 'Doctor', 'Downtown', 'Dusty', 'Fast-Talkin\'', 'Fraidy-Cat', 'Good ol\'', 'Hollerin\'', 'Lazy', 'Little', 'Mighty', 'No-', 'Preachin\'', 'Raggedy', 'Screamin\'', 'Silver', 'Smilin', 'Surly', 'Testifyin\'', 'The Reverend', 'Travelin\'', 'Uptown', 'Wailin\''];
+
+// Some all-purpose surnames...
+let bluesLast = ['Smith', 'Wilson', 'Johnson', 'King', 'Watkins', 'Jones', 'Jackson', 'Pullman', 'Williams', 'Williamson', 'McHenry', 'Washington', "Smith", "Jackson", "Walker", "Jenkins", "Lee", "Perkins", "Malone", "McLee", "McCrae", "McKinley", "Morganfield", "Logan", "Humphries", "Johnson", "Flamer", "Brown", 'Jackson', 'McGee', 'Haskins', 'Dupree', 'Green', 'Brown', 'Jones', 'Rivers', 'Wilson', 'Washington', 'Smith', 'Parker', 'Lee', 'Thompkins', 'King', 'Bradley', 'Hawkins', 'Jefferson', 'Davis', 'Franklin', 'White', 'Jenkins', 'Bailey', 'Johnson', 'Henderson', 'Allison', 'Carruthers'];
+
+if (gender  = true) {};
+
+if (gender = false) {};
+
+if (gender = null) {};
+
+
+// Add Marsalis easter egg
+
+
+*/
